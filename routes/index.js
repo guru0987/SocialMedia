33bb63bee691b357c3350b2c678def929532af04
guru0987 <nishantrana0987@gmail.com>
2021-06-23 16:58:41 +0530

    Added few more routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
 const homeController = require('../controllers/home_controller')
 
 router.get('/', homeController.home)
+router.use('/users', require('./users'));
 
-
+//for any further use access from here 
+//router.use('./routerName', require('./routerFile'));
 
 module.exports = router;
